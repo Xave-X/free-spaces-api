332c8622dee855edb287454bfd6622e6b1ec1cdf
XPS13DAVID\Work <devaney@linew.co.jp>
2026-05-04 14:21:54 +0900

    Dockerize app with Docker Compose and PostgreSQL setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,13 @@ import { TypeOrmModule } from '@nestjs/typeorm'; // DD added 20/04/2026 as I set
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'free_spaces',
+      host: process.env.DB_HOST || 'localhost', // DD changed from localhost 04/05/2026 whilst setting up Docker Compose
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_NAME || 'free_spaces',
+      retryAttempts: 10, // DD try to connect to the database 10 times
+      retryDelay: 3000,  // DD wait 3 second between attempts
       autoLoadEntities: true,
       synchronize: true,
     }), // DD added TypeOrmModule to imports on 20/04/2026 while setting up database
